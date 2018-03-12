88e32fa2718e70b4353e43ba5efadc82267a4876
BenWhite <958255724@qq.com>
2018-03-12 15:07:49 +0800

    submit  Q5

---
+++ codewars-algorithms/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: littlewhite
+@license: MIT Licence
+@contact: littlewhite0606@qq.com
+@site: https://littlebai0606.github.io/
+@software: PyCharm
+@file: __init__.py.py
+@time: 2018/3/12 下午1:59
+"""
